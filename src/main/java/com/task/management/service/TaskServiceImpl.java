61252a63e9017839b687d61a95a4da922be3b2ef
jitha <jitha.hariharan@aegon.nl>
2022-05-17 17:54:21 +0200

    added annotation

--- src/main/java/com/task/management/service/TaskServiceImpl.java
+++ src/main/java/com/task/management/service/TaskServiceImpl.java
@@ -18,29 +18,35 @@ public class TaskServiceImpl implements TaskService {
   @Autowired
   private TaskRepository taskRepository;
 
+  @Override
   public List<Task> getAllTasks() {
     List<Task> tasks = new ArrayList<>();
     taskRepository.findAll().forEach(task -> tasks.add(task));
     return tasks;
   }
 
+  @Override
   public Optional<Task> getTaskById(Long id) {
     return taskRepository.findById(id);
   }
 
+  @Override
   public Task addTask(@NotNull Task task) {
     task.setCompleted("pending");
     return taskRepository.save(task);
   }
 
+  @Override
   public Task updateTask(@NotNull Task task) {
     return taskRepository.save(task);
   }
 
+  @Override
   public void deleteTask(Long id) {
     taskRepository.deleteById(id);
   }
 
+  @Override
   public void deleteAllTasks() {
     taskRepository.deleteAll();
   }
